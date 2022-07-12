d04e604c9c18cf12172ad7f49ed4a430c1a9025d
jeremy-tesh <jermitesh20@gmail.com>
2022-07-12 13:13:40 +0300

    feat:hardhat configuration

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
